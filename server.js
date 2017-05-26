2644e8d81b3898f585aed0872e8d7a04b84ddd42
nmasamba <slimm4rl@yahoo.com>
2017-05-26 20:09:13 +0100

    dbc

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-/* jshint node: true */
+
 'use strict';
 
 var express = require('express'),
     mongoose = require('mongoose'),
-    config = require('./server/configure'),
-    app = express();
+    config = require('./server/configure');
+
+var app = express();
 
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 
 app = config(app);
 
-mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connect('mongodb://memestamdb:funmemes@candidate.4.mongolayer.com:11544,candidate.33.mongolayer.com:11745/app69394383');
 mongoose.connection.on('open', function() {
     console.log('Mongoose connected.');
 });
